9d507093a7302aaa2b4efafa796e6ec6fe7e7c63
DargunSU <loft998@yandex.ru>
2023-03-30 23:02:38 +0300

    Homework 7_34

--- Home_work/Seminar_7/HW_7_34.py
+++ Home_work/Seminar_7/HW_7_34.py
@@ -6,7 +6,27 @@
 # вбивает в программу с клавиатуры. В ответе напишите “Парам пам-пам”, если с ритмом все в порядке 
 # и “Пам парам”, если с ритмом все не в порядке
 
-string = 'пара-ра-рам ром-пюм-папам па-ра-па-дам'
+#stih = 'пара-ро-рам рам-пам-пупам па-ра-па-дам'
+stih = input('Винн-Пух говорит: ')
+glasnie = ['у','е','ы','а','о','э','я','и','ю']
+
+#count = stih_split[0].count(glasnie.strip(glasnie[0]))
+stih_split = stih.split()
+count = []
+count_word = []
+#print(stih_split[0].count('уеыаоэяию'))
+for i in stih_split:
+    count_word = [0]
+    for j in glasnie:
+        if i.count(j) != 0:
+            count_word.append(i.count(j))
+    count.append(sum(count_word))          
+print(count)
+if len(set(count)) == 1: print('Парам пам-пам')
+else: print('Пам парам')
+
+
+"""string = 'пара-ра-рам ром-пюм-папам па-ра-па-дам'
 letter = 'аеёиоуэюя'
 arr = string.split(' ')
 
@@ -22,3 +42,15 @@ if len(set(list1))==1:
     print('Парам пам-пам')
 else:
     print('Пам парам')
+    
+string = 'пара-пара-рам прамар-рамар-пар па-па-рама-ра'
+array = string.split()
+count = array[0].count('а')
+for i in range(len(array)):
+     if array[i].count('а') == count:
+         continue
+     else:
+         break
+         print("парам-пам")
+else:
+    print("парм-пам-пам")"""
